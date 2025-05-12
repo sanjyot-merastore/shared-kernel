ee836cefc646edfee568dc19b85c31d076c7a883
Sanjyot Agureddy <sanjyot.agureddy4@gmail.com>
2025-05-12 23:07:52 +0530

    MERA-46: (feat) Add ApiResponse class and update project dependencies

---
+++ src/MeraStore.Shared.Kernel.Common.Core/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeraStore.Shared.Kernel.Common.Core;
+
+public class ApiResponse<T>
+{
+  public T? Response { get; set; }
+  public int StatusCode { get; set; }
+  public ProblemDetails? ErrorInfo { get; set; }
+
+  public bool IsSuccess => StatusCode is >= 200 and < 300;
+}
