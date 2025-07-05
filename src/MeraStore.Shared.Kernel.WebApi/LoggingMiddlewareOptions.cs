2a1df3efd37b1fd6d2e280a9d8269cd8f77a812a
Sanjyot Agrueddy <sanjyot.agureddy4@gmail.com>
2025-07-05 14:07:10 +0530

    udpate logging middleware

---
+++ src/MeraStore.Shared.Kernel.WebApi/LoggingMiddlewareOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace MeraStore.Shared.Kernel.WebApi;
+
+public sealed class LoggingMiddlewareOptions
+{
+    public List<string> SkipPaths { get; init; } = [];
+    public List<string> SkipPathStartsWith { get; init; } = [];
+    public List<string> SkipMethods { get; init; } = [];
+}
